782a34382bac26a9ab08a1affcbec5769af5230d
y.khochare <y.khochare@maxxton.com>
2026-05-06 13:13:00 +0530

    Added documentation on main class using annotation.

--- Reservation-master/Reservation/src/main/java/com/example/Reservation/ReservationApplication.java
+++ Reservation-master/Reservation/src/main/java/com/example/Reservation/ReservationApplication.java
@@ -1,8 +1,17 @@
 package com.example.Reservation;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Silver Heavens Resort API",
+                version = "1.0",
+                description = "Reservation & Cancellation Management System for Silver Heavens Resort"
+        )
+)
 @SpringBootApplication
 public class ReservationApplication {
 
